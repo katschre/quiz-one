117a721454ca47e02cdfce276746d9cd4cb60cae
katschre <schreiber.katrina@gmail.com>
2016-03-20 02:52:38 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuizOne::Application.config.secret_key_base = 'c1b3215eeb17f9868c196e4f92c13f55b3cb85060b5269bd67ee2c05c214588d1ff063c1f917f09b6c9c6c6135c910583754b26b46dec1afb6370b95597bb3c7'
