32d7072d9ce4412564f06a42dd8ca16fb81db4d0
katschre <schreiber.katrina@gmail.com>
2016-03-30 12:40:14 +0000

    Adding item create

--- app/controllers/firstquiz_controller.rb
+++ app/controllers/firstquiz_controller.rb
@@ -2,4 +2,20 @@ class FirstquizController < ApplicationController
   def index
     @packings = Packing.all
   end
+
+  def new
+    @packing = Packing.new
+  end
+
+  def create
+    Packing.create(packing_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def packing_params
+    params.require(:packing).permit(:item, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 QuizOne::Application.routes.draw do
 
   root 'firstquiz#index'
+
+  resources :packings
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
